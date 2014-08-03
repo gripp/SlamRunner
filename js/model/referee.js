151eed926b093fb81251fa6567e9418cd9c0a053
Gripp <marshall@ThinkOrElse.com>
2014-08-02 20:37:56 -0400

    Tiny style fix and added a TODO.

--- js/model/referee.js
+++ js/model/referee.js
@@ -5,7 +5,9 @@ SlamRunner.Model.Referee = function() {
 };
 
 
-SlamRunner.Model.Referee.prototype.update_ = function() {};
+SlamRunner.Model.Referee.prototype.update_ = function() {
+  // TODO(gripp) Sync slam state to the cloud for automatic restores.
+};
 
 
 SlamRunner.init = function() {
