0d9ba7d5a661a906a61e7bbe024d5a9b99fdbb34
Gripp <marshall@ThinkOrElse.com>
2014-11-29 14:52:13 -0600

    Reorganized directory structure and restarted JS architecture with the Time class.

---
+++ js/model/time.js
@@ -0,0 +1,36 @@
+SlamRunner.Model.Time = function() {
+  this.time_ms_ = null;
+};
+
+
+// 10 secs * 1000 ms/sec
+SlamRunner.Model.Time.PENALTY_PERIOD_ = 10 * 1000;
+
+
+SlamRunner.Model.Time.PENALTY_POINTS_ = .5;
+
+
+// 3 min * 60 secs/min * 1000 ms/sec
+SlamRunner.Model.Time.TIME_LIMIT_ = 3 * 60 * 1000;
+
+
+SlamRunner.Model.Time.prototype.setTimeMs = function(time_ms) {
+  this.time_ms_ = time_ms;
+};
+
+
+SlamRunner.Model.Time.prototype.getTimeMs = function() {
+  return this.time_ms_;
+};
+
+
+SlamRunner.Model.Time.prototype.getTimePenalty = function() {
+  var overflowTime = this.getTimeMs() - SlamRunner.Model.Time.TIME_LIMIT_;
+  if (overflowTime < 0) {
+    return 0;
+  } else {
+    return (
+        Math.floor(overflowTime / SlamRunner.Model.Time.PENALTY_PERIOD_) *
+        SlamRunner.Model.Time.PENALTY_POINTS_);
+  }
+};
