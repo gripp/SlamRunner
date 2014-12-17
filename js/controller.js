223dc7ee6688b40968a3a5cdda9fae3ba886d20d
gripp <marshall@thinkorelse.com>
2014-12-17 19:40:05 +0000

    Rebuilt the timer controller and wired it to the view.

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,12 @@
 SlamRunner.Controller = function() {
+  this.timer_ = new SlamRunner.Controller.Timer();
+
   document.addEventListener(
       SlamRunner.Model.Slam.Event.STARTED, this.initializePage_.bind(this));
   document.addEventListener(
       SlamRunner.Model.Slam.Event.UPDATED, this.updateViewFromModel_.bind(this));
+  document.addEventListener(
+      SlamRunner.Model.Slam.Event.TIME_UPDATED_EVENT, this.updateModelFromView_.bind(this));
 
   this.slam_ = new SlamRunner.Model.Slam();
   // TODO(gripp): We have to actually initialize the slam here. For now, a fake
@@ -13,19 +17,26 @@ SlamRunner.Controller = function() {
 
 
 SlamRunner.Controller.prototype.initializePage_ = function() {
-  // Set up event firers.
+  // Set up score input event tiggers.
   var scoreBoxes = document.getElementsByClassName(
-      SlamRunner.Controller.Css_.SCORE_INPUT);
+      SlamRunner.Controller.HtmlNames_.SCORE_INPUT);
   for (var i = 0; i < scoreBoxes.length; i++) {
     scoreBoxes[i].onchange = this.updateModelFromView_.bind(this);
     scoreBoxes[i].disabled = false;
   }
+
+  // Enable the timer.
+  this.timer_.enable();
 };
 
 
-SlamRunner.Controller.Css_ = {
+SlamRunner.Controller.HtmlNames_ = {
+    RESET_BUTTON: 'reset-button',
     SCORE_INPUT: 'score',
     SCORE_TOTAL: 'total-score',
+    START_STOP_BUTTON: 'start-stop-button',
+    TIMER_FACE: 'timer-face',
+    TIMER_FACE_ERROR: 'timer-face-error',
 };
 
 
@@ -37,7 +48,7 @@ SlamRunner.Controller.Events = {
 SlamRunner.Controller.prototype.updateModelFromView_ = function() {
   // Update scores.
   var scoreBoxes = document.getElementsByClassName(
-      SlamRunner.Controller.Css_.SCORE_INPUT);
+      SlamRunner.Controller.HtmlNames_.SCORE_INPUT);
   var scores = [];
   for (var i = 0; i < scoreBoxes.length; i++) {
     var currentScore = parseFloat(scoreBoxes[i].value);
@@ -52,12 +63,14 @@ SlamRunner.Controller.prototype.updateModelFromView_ = function() {
 
   this.slam_.getCurrentPoet().addOrUpdateScoresByRound(
       scores, this.slam_.getCurrentRound());
+
+  // TODO(gripp) Update the time of the current poem.
 };
 
 
 SlamRunner.Controller.prototype.updateViewFromModel_ = function() {
   var scoreBoxes = document.getElementsByClassName(
-      SlamRunner.Controller.Css_.SCORE_INPUT);
+      SlamRunner.Controller.HtmlNames_.SCORE_INPUT);
   var totalScore = this.slam_.getCurrentPoet().getScoreByRound(
       this.slam_.getCurrentRound()).getTotalScore().toString();
   var scores = this.slam_.getCurrentPoet().getScoreByRound(
@@ -73,7 +86,7 @@ SlamRunner.Controller.prototype.updateViewFromModel_ = function() {
   }
 
   document.getElementById(
-      SlamRunner.Controller.Css_.SCORE_TOTAL).textContent =
+      SlamRunner.Controller.HtmlNames_.SCORE_TOTAL).textContent =
           totalScore.indexOf('.') < 0 ? totalScore + '.0' : totalScore;
 };
 
---
+++ js/controller/timer.js
@@ -0,0 +1,102 @@
+SlamRunner.Controller.Timer = function() {
+  this.resetButton_ = document.getElementById(
+      SlamRunner.Controller.HtmlNames_.RESET_BUTTON);
+  this.startStopButton_ = document.getElementById(
+      SlamRunner.Controller.HtmlNames_.START_STOP_BUTTON);
+  this.timerFace_ = document.getElementById(
+      SlamRunner.Controller.HtmlNames_.TIMER_FACE);
+
+  this.startTime_ = null;
+  this.stopTime_ = null;
+
+  this.startStopButton_.onclick = this.onStartStopClick_.bind(this);
+  this.resetButton_.onclick = this.onResetClick_.bind(this);
+  setInterval(
+      (function() {
+         if (this.startTime_ && !this.stopTime_) {
+           this.updateTimerFace_();
+         }
+      }).bind(this), 1);
+};
+
+
+SlamRunner.Controller.Timer.START_LABEL_ = 'Start';
+SlamRunner.Controller.Timer.STOP_LABEL_ = 'Stop';
+SlamRunner.Controller.Timer.TIME_UPDATED_EVENT = 'timeupdated';
+
+
+SlamRunner.Controller.Timer.prototype.enable = function() {
+  this.startStopButton_.disabled = false;
+  this.resetButton_.disabled = false;
+};
+
+
+SlamRunner.Controller.Timer.prototype.onResetClick_ = function() {
+  this.startTime_ = null;
+  this.stopTime_ = null;
+  this.updateTimerFace_();
+  document.dispatchEvent(
+      new Event(SlamRunner.Controller.Timer.TIME_UPDATED_EVENT));
+};
+
+
+SlamRunner.Controller.Timer.prototype.onStartStopClick_ = function() {
+  if (!this.startTime_) {
+    this.startTime_ = new Date();
+    this.startStopButton_.textContent =
+        SlamRunner.Controller.Timer.STOP_LABEL_;
+  } else if (this.stopTime_) {
+    this.startTime_ = new Date(
+        Math.abs(new Date() - (this.stopTime_ - this.startTime_)));
+    this.stopTime_ = null;
+    this.startStopButton_.textContent =
+        SlamRunner.Controller.Timer.STOP_LABEL_;
+  } else {
+    this.updateTimerFace_();
+    this.stopTime_ = new Date();
+    this.startStopButton_.textContent =
+        SlamRunner.Controller.Timer.START_LABEL_;
+    document.dispatchEvent(
+        new Event(SlamRunner.Controller.Timer.TIME_UPDATED_EVENT));
+  }
+};
+
+
+SlamRunner.Controller.Timer.prototype.setPenaltyHighlightEnabled =
+    function(enabled) {
+  this.timerFace_.className =
+      enabled ?
+      SlamRunner.Controller.HtmlNames_.TIMER_FACE_ERROR :
+      '';
+};
+
+
+SlamRunner.Controller.Timer.prototype.timerTick_ = function() {
+  if (this.startTime_ && !this.stopTime_) {
+    this.updateTimerFace_();
+  }
+};
+
+
+SlamRunner.Controller.Timer.prototype.updateTimerFace_ = function() {
+  var time;
+
+  if (this.startTime_) {
+    time = new Date(Math.abs(new Date() - this.startTime_));
+  } else {
+    time = new Date(0);
+  }
+
+  var minutes = time.getMinutes().toString();
+  minutes = minutes.length == 1 ? "0" + minutes : minutes;
+
+  var seconds = time.getSeconds().toString();
+  seconds = seconds.length == 1 ? "0" + seconds : seconds;
+
+  var milliseconds = time.getMilliseconds().toString();
+  while (milliseconds.length < 3) {
+    milliseconds = "0" + milliseconds;
+  }
+
+  this.timerFace_.textContent = minutes + ":" + seconds + "." + milliseconds;
+};
